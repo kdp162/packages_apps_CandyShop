Project-Xtended/packages_apps_Xtensions

Add ability to permanently hide apps from recents [3/3]
Change-Id: I1afcab02d4daa1f2f01bc537fca9878a1eb0ad6b
Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

src/com/msm/xtended/fragments/hideappfromrecents/HAFRAppChooserDialog.java
@@ -1,0 +1,104 @@
+package com.msm.xtended.fragments.hideappfromrecents;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.inputmethod.EditorInfo;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.ImageButton;
+import android.view.KeyEvent;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+public abstract class HAFRAppChooserDialog extends Dialog {
+
+    final HAFRAppChooserAdapter dAdapter;
+    final ProgressBar dProgressBar;
+    final ListView dListView;
+    final EditText dSearch;
+    final ImageButton dButton;
+
+    private int mId;
+
+    public HAFRAppChooserDialog(Context context) {
+        super(context);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_app_chooser_list);
+
+        dListView = (ListView) findViewById(R.id.listView1);
+        dSearch = (EditText) findViewById(R.id.searchText);
+        dButton = (ImageButton) findViewById(R.id.searchButton);
+        dProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
+
+        dAdapter = new HAFRAppChooserAdapter(context) {
+            @Override
+            public void onStartUpdate() {
+                dProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onFinishUpdate() {
+                dProgressBar.setVisibility(View.GONE);
+            }
+        };
+
+        dListView.setAdapter(dAdapter);
+        dListView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
+                HAFRAppChooserAdapter.AppItem info = (HAFRAppChooserAdapter.AppItem) av
+                        .getItemAtPosition(pos);
+                onListViewItemClick(info, mId);
+                dismiss();
+            }
+        });
+
+        dButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dAdapter.getFilter().filter(dSearch.getText().toString(), new Filter.FilterListener() {
+                    public void onFilterComplete(int count) {
+                        dAdapter.update();
+                    }
+                });
+            }
+        });
+
+        dSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    dAdapter.getFilter().filter(dSearch.getText().toString(), new Filter.FilterListener() {
+                        public void onFilterComplete(int count) {
+                            dAdapter.update();
+                        }
+                    });
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        dAdapter.update();
+    }
+
+    public void show(int id) {
+        mId = id;
+        show();
+    }
+
+    public void setLauncherFilter(boolean enabled) {
+        dAdapter.setLauncherFilter(enabled);
+    }
+
+    public abstract void onListViewItemClick(HAFRAppChooserAdapter.AppItem info, int id);
+}
+
+

src/com/msm/xtended/fragments/hideappfromrecents/HAFRAppListAdapter.java
@@ -1,0 +1,153 @@
+package com.msm.xtended.fragments.hideappfromrecents;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+import java.util.LinkedList;
+import java.util.Map;
+
+public abstract class HAFRAppListAdapter extends BaseAdapter {
+
+    final Context mContext;
+    final Handler mHandler;
+    final PackageManager mPackageManager;
+    final LayoutInflater mLayoutInflater;
+
+    protected LinkedList<PackageItem> mApps = new LinkedList<PackageItem>();
+
+    public HAFRAppListAdapter(Context context, Map<String, Integer> list) {
+        mContext = context;
+        mHandler = new Handler();
+        mPackageManager = context.getPackageManager();
+        mLayoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        update(list);
+    }
+
+    public void update(final Map<String, Integer> app_array) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (mApps) {
+                    final PackageItem[] array = new PackageItem[app_array.size() * 2];
+                    for (String pkg_name : app_array.keySet()) {
+                        try {
+                            ApplicationInfo ai = mPackageManager.getApplicationInfo(pkg_name, 0);
+                            final PackageItem item = new PackageItem();
+                            item.title = ai.loadLabel(mPackageManager);
+                            item.icon = ai.loadIcon(mPackageManager);
+                            item.packageName = ai.packageName;
+                            array[app_array.get(pkg_name)] = item;
+                        } catch (Exception e) {
+                        }
+                    }
+
+                    final LinkedList<PackageItem> temp = new LinkedList<PackageItem>();
+                    for (int x = 0; x < array.length; x++) {
+                        if (array[x] != null) {
+                            temp.add(array[x]);
+                        }
+                    }
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mApps.clear();
+                            mApps = temp;
+                            notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public int getCount() {
+        return mApps.size();
+    }
+
+    public LinkedList<PackageItem> getList() {
+        return mApps;
+    }
+
+    @Override
+    public PackageItem getItem(int position) {
+        return mApps.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        if (position < 0 || position >= mApps.size()) {
+            return -1;
+        }
+        return mApps.get(position).hashCode();
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mLayoutInflater.inflate(R.layout.view_package_list, parent, false);
+            holder = new ViewHolder();
+            holder.name = (TextView) convertView.findViewById(android.R.id.title);
+            holder.icon = (ImageView) convertView.findViewById(android.R.id.icon);
+            holder.pkg = (TextView) convertView.findViewById(android.R.id.message);
+            holder.remove = (ImageButton) convertView.findViewById(R.id.removeButton);
+            convertView.setTag(holder);
+        }
+        final PackageItem appInfo = getItem(position);
+
+        holder.name.setText(appInfo.title);
+        holder.pkg.setText(appInfo.packageName);
+        holder.icon.setImageDrawable(appInfo.icon);
+        holder.remove.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRemoveButtonPress(appInfo);
+            }
+        });
+        return convertView;
+    }
+
+    public abstract void onRemoveButtonPress(PackageItem app_info);
+
+    public class PackageItem implements Comparable<PackageItem> {
+        public CharSequence title;
+        public String packageName;
+        public Drawable icon;
+
+        @Override
+        public int compareTo(PackageItem another) {
+            return this.title.toString().compareTo(another.title.toString());
+        }
+    }
+
+    static class ViewHolder {
+        TextView name;
+        ImageView icon;
+        TextView pkg;
+        ImageButton remove;
+    }
+}
+
+

kdp162/packages_apps_CandyShop

src/org/candy/candyshop/fragments/hideappfromrecents/HAFRAppChooserDialog.java
@@ -1,0 +1,104 @@
+package org.candy.candyshop.fragments.hideappfromrecents;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.inputmethod.EditorInfo;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.ImageButton;
+import android.view.KeyEvent;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+public abstract class HAFRAppChooserDialog extends Dialog {
+
+    final HAFRAppChooserAdapter dAdapter;
+    final ProgressBar dProgressBar;
+    final ListView dListView;
+    final EditText dSearch;
+    final ImageButton dButton;
+
+    private int mId;
+
+    public HAFRAppChooserDialog(Context context) {
+        super(context);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_app_chooser_list);
+
+        dListView = (ListView) findViewById(R.id.listView1);
+        dSearch = (EditText) findViewById(R.id.searchText);
+        dButton = (ImageButton) findViewById(R.id.searchButton);
+        dProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
+
+        dAdapter = new HAFRAppChooserAdapter(context) {
+            @Override
+            public void onStartUpdate() {
+                dProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onFinishUpdate() {
+                dProgressBar.setVisibility(View.GONE);
+            }
+        };
+
+        dListView.setAdapter(dAdapter);
+        dListView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
+                HAFRAppChooserAdapter.AppItem info = (HAFRAppChooserAdapter.AppItem) av
+                        .getItemAtPosition(pos);
+                onListViewItemClick(info, mId);
+                dismiss();
+            }
+        });
+
+        dButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dAdapter.getFilter().filter(dSearch.getText().toString(), new Filter.FilterListener() {
+                    public void onFilterComplete(int count) {
+                        dAdapter.update();
+                    }
+                });
+            }
+        });
+
+        dSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    dAdapter.getFilter().filter(dSearch.getText().toString(), new Filter.FilterListener() {
+                        public void onFilterComplete(int count) {
+                            dAdapter.update();
+                        }
+                    });
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        dAdapter.update();
+    }
+
+    public void show(int id) {
+        mId = id;
+        show();
+    }
+
+    public void setLauncherFilter(boolean enabled) {
+        dAdapter.setLauncherFilter(enabled);
+    }
+
+    public abstract void onListViewItemClick(HAFRAppChooserAdapter.AppItem info, int id);
+}
+
+

src/org/candy/candyshop/fragments/hideappfromrecents/HAFRAppListAdapter.java
@@ -1,0 +1,153 @@
+package org.candy.candyshop.fragments.hideappfromrecents;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+import java.util.LinkedList;
+import java.util.Map;
+
+public abstract class HAFRAppListAdapter extends BaseAdapter {
+
+    final Context mContext;
+    final Handler mHandler;
+    final PackageManager mPackageManager;
+    final LayoutInflater mLayoutInflater;
+
+    protected LinkedList<PackageItem> mApps = new LinkedList<PackageItem>();
+
+    public HAFRAppListAdapter(Context context, Map<String, Integer> list) {
+        mContext = context;
+        mHandler = new Handler();
+        mPackageManager = context.getPackageManager();
+        mLayoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        update(list);
+    }
+
+    public void update(final Map<String, Integer> app_array) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (mApps) {
+                    final PackageItem[] array = new PackageItem[app_array.size() * 2];
+                    for (String pkg_name : app_array.keySet()) {
+                        try {
+                            ApplicationInfo ai = mPackageManager.getApplicationInfo(pkg_name, 0);
+                            final PackageItem item = new PackageItem();
+                            item.title = ai.loadLabel(mPackageManager);
+                            item.icon = ai.loadIcon(mPackageManager);
+                            item.packageName = ai.packageName;
+                            array[app_array.get(pkg_name)] = item;
+                        } catch (Exception e) {
+                        }
+                    }
+
+                    final LinkedList<PackageItem> temp = new LinkedList<PackageItem>();
+                    for (int x = 0; x < array.length; x++) {
+                        if (array[x] != null) {
+                            temp.add(array[x]);
+                        }
+                    }
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mApps.clear();
+                            mApps = temp;
+                            notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public int getCount() {
+        return mApps.size();
+    }
+
+    public LinkedList<PackageItem> getList() {
+        return mApps;
+    }
+
+    @Override
+    public PackageItem getItem(int position) {
+        return mApps.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        if (position < 0 || position >= mApps.size()) {
+            return -1;
+        }
+        return mApps.get(position).hashCode();
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mLayoutInflater.inflate(R.layout.view_package_list, parent, false);
+            holder = new ViewHolder();
+            holder.name = (TextView) convertView.findViewById(android.R.id.title);
+            holder.icon = (ImageView) convertView.findViewById(android.R.id.icon);
+            holder.pkg = (TextView) convertView.findViewById(android.R.id.message);
+            holder.remove = (ImageButton) convertView.findViewById(R.id.removeButton);
+            convertView.setTag(holder);
+        }
+        final PackageItem appInfo = getItem(position);
+
+        holder.name.setText(appInfo.title);
+        holder.pkg.setText(appInfo.packageName);
+        holder.icon.setImageDrawable(appInfo.icon);
+        holder.remove.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRemoveButtonPress(appInfo);
+            }
+        });
+        return convertView;
+    }
+
+    public abstract void onRemoveButtonPress(PackageItem app_info);
+
+    public class PackageItem implements Comparable<PackageItem> {
+        public CharSequence title;
+        public String packageName;
+        public Drawable icon;
+
+        @Override
+        public int compareTo(PackageItem another) {
+            return this.title.toString().compareTo(another.title.toString());
+        }
+    }
+
+    static class ViewHolder {
+        TextView name;
+        ImageView icon;
+        TextView pkg;
+        ImageButton remove;
+    }
+}
+
+