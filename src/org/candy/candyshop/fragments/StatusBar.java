40ee72432c393e6d52c461025d4d1b5d7e712a96
kevin princiotta <kevinprinciotta@gmail.com>
2020-01-28 17:26:08 -0500

    CandyShop: Add battery styles and customizations for Android 10 [2/2]
    Allows the user to tune:
    - battery styles
    - battery percentage
    - battery charging text
    
    spezi77:
    - Adapted to our battery styles
    - Note to myself: In Android10 battery percentage has become a global setting
    
    Change-Id: I5a962d2eaab5712662c6fd4557337d4ba4df45e2

--- src/org/candy/candyshop/fragments/StatusBar.java
+++ src/org/candy/candyshop/fragments/StatusBar.java
@@ -37,6 +37,21 @@ import java.util.Collections;
 public class StatusBar extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+    private static final String STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
+    private static final String BATTERY_PERCENTAGE_HIDDEN = "0";
+    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+    private static final int BATTERY_STYLE_Q = 0;
+    private static final int BATTERY_STYLE_DOTTED_CIRCLE = 1;
+    private static final int BATTERY_STYLE_CIRCLE = 2;
+    private static final int BATTERY_STYLE_TEXT = 3;
+    private static final int BATTERY_STYLE_HIDDEN = 4;
+
+    private ListPreference mBatteryPercent;
+    private ListPreference mBatteryStyle;
+    private SwitchPreference mBatteryCharging;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -45,11 +60,37 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
+        mBatteryPercent = (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+        mBatteryCharging = (SwitchPreference) findPreference(STATUS_BAR_BATTERY_TEXT_CHARGING);
+        mBatteryStyle = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
+        int batterystyle = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_Q);
+        mBatteryStyle.setOnPreferenceChangeListener(this);
+
+        updateBatteryOptions(batterystyle);
+
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mBatteryStyle) {
+               int value = Integer.parseInt((String) newValue);
+            updateBatteryOptions(value);
+            return true;
+        }
         return false;
+    }
+
+     private void updateBatteryOptions(int batterystyle) {
+        boolean enabled = batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN;
+        if (batterystyle == BATTERY_STYLE_HIDDEN) {
+            mBatteryPercent.setValue(BATTERY_PERCENTAGE_HIDDEN);
+            mBatteryPercent.setSummary(mBatteryPercent.getEntry());
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0);
+        }
+        mBatteryCharging.setEnabled(enabled);
+        mBatteryPercent.setEnabled(enabled);
     }
 
     @Override
